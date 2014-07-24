50ef9b9ce80dafaca57d1bef738500b71be7389a
nicknish <n.nishiguchi9@gmail.com>
2014-07-23 21:30:18 -0700

    attempt at recursion

--- js/script.js
+++ js/script.js
@@ -13,84 +13,16 @@ app.controller('tttCtrl', function($scope, $firebase) {
     // Sweet Dynamic Banner
     $scope.bannerText = "Take a moment to relax.";
 
-    // Blue Variables
-    var blueDiagonalLeftRight = [],
-        blueDiagonalRightLeft = [],
-        blueColumnOne = [],
-        blueColumnTwo = [],
-        blueColumnThree = [],
-        blueRowOne = [],
-        blueRowTwo = [],
-        blueRowThree = [];
-
-    // Green Variables
-    var greenDiagonalLeftRight = [],
-        greenDiagonalRightLeft = [],
-        greenColumnOne = [],
-        greenColumnTwo = [],
-        greenColumnThree = [],
-        greenRowOne = [],
-        greenRowTwo = [],
-        greenRowThree = [];
-
     // Selection Function, Passing in individual box object
     $scope.isSelected = function(box) {
 
         // Blue Stuff!
         if (count === 11) { // If all the tiles are filled without a winner, then cat's game
-            $scope.bannerText = "cat's game!";
+            // $scope.bannerText = "cat's game!";
         } else if (count % 2 === 0 && box.color === "" && box.color != "green") {
             box.color = "blue";
             count++;
             $scope.bannerText = "green's turn";
-            // Check Row 1
-            if ((box.row === 1 && box.column === 1) || (box.row === 1 && box.column === 2) ||
-                (box.row === 1 && box.column === 3)) {
-                blueRowOne.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueRowOne);
-            }
-            // Check Row 2
-            if ((box.row === 2 && box.column === 1) || (box.row === 2 && box.column === 2) ||
-                (box.row === 2 && box.column === 3)) {
-                blueRowTwo.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueRowTwo);
-            }
-            // Check Row 3
-            if ((box.row === 3 && box.column === 1) || (box.row === 3 && box.column === 2) ||
-                (box.row === 3 && box.column === 3)) {
-                blueRowThree.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueRowThree);
-            }
-            // Check Column 1
-            if ((box.row === 1 && box.column === 1) || (box.row === 2 && box.column === 1) ||
-                (box.row === 3 && box.column === 1)) {
-                blueColumnOne.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueColumnOne);
-            }
-            // Check Column 2
-            if ((box.row === 1 && box.column === 2) || (box.row === 2 && box.column === 2) ||
-                (box.row === 3 && box.column === 2)) {
-                blueColumnTwo.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueColumnTwo);
-            }
-            // Check Column 3
-            if ((box.row === 1 && box.column === 3) || (box.row === 2 && box.column === 3) ||
-                (box.row === 3 && box.column === 3)) {
-                blueColumnThree.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueColumnThree);
-            }
-            // Check Diagonal Left-to-Right
-            if ((box.row === 1 && box.column === 1) || (box.row === 2 && box.column === 2) ||
-                (box.row === 3 && box.column === 3)) {
-                blueDiagonalLeftRight.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueDiagonalLeftRight);
-            }
-            // Check Diagonal Right-to-Left
-            if ((box.row === 1 && box.column === 3) || (box.row === 2 && box.column === 2) ||
-                (box.row === 3 && box.column === 1)) {
-                blueDiagonalRightLeft.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, blueDiagonalRightLeft);
-            }
         }
         // Green Stuff! 
         else if ((count % 3 === 0 || count / 5 === 1 || count / 7 === 1) &&
@@ -98,91 +30,32 @@ app.controller('tttCtrl', function($scope, $firebase) {
             box.color = "green";
             count++;
             $scope.bannerText = "blue's turn";
-            // Check Row 1
-            if ((box.row === 1 && box.column === 1) || (box.row === 1 && box.column === 2) ||
-                (box.row === 1 && box.column === 3)) {
-                greenRowOne.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenRowOne);
-            }
-            // Check Row 2
-            if ((box.row === 2 && box.column === 1) || (box.row === 2 && box.column === 2) ||
-                (box.row === 2 && box.column === 3)) {
-                greenRowTwo.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenRowTwo);
-            }
-            // Check Row 3
-            if ((box.row === 3 && box.column === 1) || (box.row === 3 && box.column === 2) ||
-                (box.row === 3 && box.column === 3)) {
-                greenRowThree.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenRowThree);
-            }
-            // Check Column 1
-            if ((box.row === 1 && box.column === 1) || (box.row === 2 && box.column === 1) ||
-                (box.row === 3 && box.column === 1)) {
-                greenColumnOne.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenColumnOne);
-            }
-            // Check Column 2
-            if ((box.row === 1 && box.column === 2) || (box.row === 2 && box.column === 2) ||
-                (box.row === 3 && box.column === 2)) {
-                greenColumnTwo.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenColumnTwo);
-            }
-            // Check Column 3
-            if ((box.row === 1 && box.column === 3) || (box.row === 2 && box.column === 3) ||
-                (box.row === 3 && box.column === 3)) {
-                greenColumnThree.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenColumnThree);
-            }
-            // Check Diagonal Left-to-Right
-            if ((box.row === 1 && box.column === 1) || (box.row === 2 && box.column === 2) ||
-                (box.row === 3 && box.column === 3)) {
-                greenDiagonalLeftRight.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenDiagonalLeftRight);
-            }
-            // Check Diagonal Right-to-Left
-            if ((box.row === 1 && box.column === 3) || (box.row === 2 && box.column === 2) ||
-                (box.row === 3 && box.column === 1)) {
-                greenDiagonalRightLeft.push("(" + box.row + ", " + box.column + ")");
-                checkFtw(box, greenDiagonalRightLeft);
-            }
         } else {
             $scope.bannerText = "this has been selected"; // Maybe have a dynamically updated prompt.
         }
     };
 
-    // Check for a win combo by length row/col/diag array
-    var checkFtw = function(box, someArray) {
-        if (someArray.length === 3) {
-            $scope.bannerText = box.color + " wins!";
-        }
-    };
 
-    // Reset Game - reset values to default
-    $scope.resetGame = function() {
-        $scope.bannerText = "Game reset.";
-        count = 2;
-        blueDiagonalLeftRight.length = 0;
-        blueDiagonalRightLeft.length = 0;
-        blueColumnOne.length = 0;
-        blueColumnTwo.length = 0;
-        blueColumnThree.length = 0;
-        blueRowOne.length = 0;
-        blueRowTwo.length = 0;
-        blueRowThree.length = 0;
-        greenDiagonalLeftRight.length = 0;
-        greenDiagonalRightLeft.length = 0;
-        greenColumnOne.length = 0;
-        greenColumnTwo.length = 0;
-        greenColumnThree.length = 0;
-        greenRowOne.length = 0;
-        greenRowTwo.length = 0;
-        greenRowThree.length = 0;
-        // Overwrite the color property on each box
-        for (i = 0; i < $scope.boxes.length; i++) {
-            $scope.boxes[i].color = "";
+    var checkRow = function(row, column) {
+        // create default values
+        if (row === null) {
+            row = 0;
         }
-    };
+        if (column === null) {
+            column = 0;
+        }
+
+        // start from the topleft row, topleft col
+        if (row <= 1) {
+            if (($scope.boxes[row].color === $scope.boxes[row + 1].color) &&
+                $scope.boxes[row].color != "") {
+                $()
+            }
+        }
+
+
+    }
+
 
     // JavaScript Objects to build out Gameboard
     $scope.boxes = [{
